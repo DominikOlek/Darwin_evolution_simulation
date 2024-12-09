f30faf892ff305714698bd3addd6d34aeed00d48
domin <dominikkomar3@gmail.com>
2024-12-09 20:04:30 +0100

    cw1

---
+++ ProjektPO/src/main/java/agh/ics/oop/Models/MoveDirection.java
@@ -0,0 +1,12 @@
+package agh.ics.oop.Models;
+
+public enum MoveDirection{
+    FORWARD,
+    FORWARD_RIGHT,
+    BACKWARD,
+    BACKWARD_RIGHT,
+    LEFT,
+    LEFT_RIGHT,
+    RIGHT,
+    RIGHT_RIGHT;
+}
